79c4a9c62b350123ea85a172c61da4e286c1f72f
Arnaud Bandonkeye <bandonkeyea@gmail.com>
2022-10-20 16:07:26 -0600

    Migrate the user in database

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2022_10_20_195204) do
+ActiveRecord::Schema[7.0].define(version: 2022_10_20_215732) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -22,6 +22,14 @@ ActiveRecord::Schema[7.0].define(version: 2022_10_20_195204) do
     t.string "color"
     t.string "image"
     t.boolean "rented"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  create_table "users", force: :cascade do |t|
+    t.string "name"
+    t.string "photo"
+    t.date "date_of_birth"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
