1d8d0fd3ba9a7c57d11d5aa9141de785dff9581c
David Vera Castillo <indigodavid@gmail.com>
2022-10-24 11:12:52 -0500

    🎛 Customize Registrations and Sessions controllers

---
+++ app/controllers/users/registrations_controller.rb
@@ -0,0 +1,11 @@
+# frozen_string_literal: true
+
+class Users::RegistrationsController < Devise::RegistrationsController
+  respond_to :json
+  def create
+    build_resource(sign_up_params)
+    resource.save
+    sign_in(resource_name, resource)
+    render json: resource
+  end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,16 @@
 Rails.application.routes.draw do
-  devise_for :users
   get 'private/test'
+  devise_for :users, 
+    path: '', 
+    path_names: {
+      sign_in: 'login',
+      sign_out: 'logout',
+      registration: 'signup'
+    },
+    controllers: {
+      sessions: 'users/sessions',
+      registrations: 'users/registrations'
+    }
   namespace :api do
     namespace :v1 do
       resources :cars
