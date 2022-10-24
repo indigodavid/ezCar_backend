84ffc833c02747d51ca4c8683fa29acb235bf7b0
David Vera Castillo <indigodavid@gmail.com>
2022-10-24 12:33:34 -0500

    ✅ Add JWT auth, validate_age method, and reservations

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,6 +1,19 @@
 class User < ApplicationRecord
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
-  devise :database_authenticatable, :registerable,
-         :recoverable, :rememberable, :validatable
+  devise :database_authenticatable, :registerable, 
+  :jwt_authenticatable, jwt_revocation_strategy: JwtDenylist
+
+  validates :name, :date_of_birth, presence: true
+  validate :validate_age
+
+  has_many :reservations
+
+  private
+
+  def validate_age
+    if date_of_birth.present? && date_of_birth > 18.years.ago.to_date
+      errors.add(:date_of_birth, 'You should be over 18 years old.')
+    end
+  end
 end
