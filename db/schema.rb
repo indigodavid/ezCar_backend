e69c693f2d2ba9c3002373908ce6635a2ffb5c74
Arnaud Bandonkeye <bandonkeyea@gmail.com>
2022-10-24 09:16:18 -0600

    Create reservations table and migrate it

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2022_10_20_215732) do
+ActiveRecord::Schema[7.0].define(version: 2022_10_21_210853) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -26,6 +26,15 @@ ActiveRecord::Schema[7.0].define(version: 2022_10_20_215732) do
     t.datetime "updated_at", null: false
   end
 
+  create_table "reservations", force: :cascade do |t|
+    t.date "reservation_date"
+    t.date "due_date"
+    t.bigint "car_id", null: false
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["car_id"], name: "index_reservations_on_car_id"
+  end
+
   create_table "users", force: :cascade do |t|
     t.string "name"
     t.string "photo"
@@ -34,4 +43,5 @@ ActiveRecord::Schema[7.0].define(version: 2022_10_20_215732) do
     t.datetime "updated_at", null: false
   end
 
+  add_foreign_key "reservations", "cars"
 end
