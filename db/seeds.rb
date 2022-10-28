a0d91c4a2fb2a5edc6a122fd57d22735f05896b9
darikmohammed <r.m.darik@gmail.com>
2022-10-28 20:09:19 +0300

    Update seed file

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,6 +5,18 @@
 #
 #   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
 #   Character.create(name: "Luke", movie: movies.first)
-User.create(name: 'David', email: 'david@gmail.com', password: '123456', password_confirmation: '123456', photo: 'url', date_of_birth: '1987-03-20')
-Car.create(name: 'Mercedes G550', car_type: 'SUV', brand: 'Mercedes', fee_per_day: 40.20, color: '#000000', image: 'some url', rented: false)
-Reservation.create(user_id: 1, car_id: 1, reservation_date: '2022_11_01', due_date: '2022_11_04')
+User.create(name: 'David', email: 'david@gmail.com', password: '123456', password_confirmation: '123456', photo: 'url_david',
+            date_of_birth: '1987-03-20')
+User.create(name: 'Julio', email: 'julio@gmail.com', password: '123456', password_confirmation: '123456', photo: 'url_julio',
+            date_of_birth: '1987-03-20')
+User.create(name: 'Darik', email: 'darik@gmail.com', password: '123456', password_confirmation: '123456',
+            photo: 'url_darik', date_of_birth: '1987-03-20')
+User.create(name: 'Arnaud', email: 'arnaud@gmail.com', password: '123456', password_confirmation: '123456',
+            photo: 'url_arnaud', date_of_birth: '1987-03-20')
+Car.create(name: 'Mercedes G550', car_type: 'SUV', brand: 'Mercedes', fee_per_day: 40.20, color: '#000000',
+           image: 'some url', rented: false)
+@user = User.first
+@car = Car.first
+Reservation.create(user: @user, car: @car, reservation_date: '2022-11-01', due_date: '2022-11-04')
+User.create(name: 'Admin', email: 'admin@gmail.com', password: '123456', password_confirmation: '123456', photo: 'url',
+            date_of_birth: '1987-03-20', role: 'admin')
