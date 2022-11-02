901602cfe835b04ff21201e3e07fd83fe1e1bd7c
David Vera Castillo <indigodavid@gmail.com>
2022-11-02 12:41:13 -0500

    ✅ Add ImageKit Keys to production environment

--- config/initializers/imagekitio.rb
+++ config/initializers/imagekitio.rb
@@ -1,5 +1,5 @@
 ImageKitIo.configure do |config|
-  if Rails.env.development?
+  if Rails.env.development? || Rails.env.production?
     config.public_key = 'public_eInAT404MVNpMKyc+zehqaVtmN0='
     config.private_key = 'private_Oh+gNMu+S3YH64ZDnI22XNg9tm0='
     config.url_endpoint = 'https://ik.imagekit.io/ezcar' # https://ik.imagekit.io/your_imagekit_id
