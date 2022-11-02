74b7151b8335e738db471f0ffe166dc1786e8cd8
David Vera Castillo <indigodavid@gmail.com>
2022-11-01 22:29:41 -0500

    🎛 Configure ImageKit Keys and endpoint

---
+++ config/initializers/imagekitio.rb
@@ -0,0 +1,9 @@
+ImageKitIo.configure do |config|
+  if Rails.env.development?
+    config.public_key = 'public_eInAT404MVNpMKyc+zehqaVtmN0='
+    config.private_key = 'private_Oh+gNMu+S3YH64ZDnI22XNg9tm0='
+    config.url_endpoint = 'https://ik.imagekit.io/ezcar' # https://ik.imagekit.io/your_imagekit_id
+  end
+  config.service = :active_storage
+  # config.constants.MISSING_PRIVATE_KEY = 'custom error message'
+end
